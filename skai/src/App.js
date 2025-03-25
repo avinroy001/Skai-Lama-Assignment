75544f8c7adf8021407a7f1d753d83afcd05f83f
Avinash <avinash.roy@solytics-partners.com>
2025-03-25 21:23:39 +0530

    backend started

--- skai/src/App.js
+++ skai/src/App.js
@@ -1,13 +1,13 @@
 // import './App.css';
 import { Route, Routes } from 'react-router-dom';
-// import Landing from './Landing/Landing';
+import Landing from './Landing/Landing';
 // import HomePage from './HomePages/HomePage';
-import UploadFlowOne from './UploadFlow/UploadFlowOne';
+// import UploadFlowOne from './UploadFlow/UploadFlowOne';
 
 function App() {
   return (
     <Routes>
-     <Route path='/' element={ <UploadFlowOne/> }/>
+     <Route path='/' element={ <Landing/> }/>
     </Routes>
   );
 }
--- skai/src/Landing/Landing.jsx
+++ skai/src/Landing/Landing.jsx
@@ -1,42 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Landing.css";
 import Logo from "../Assets/QuesLogo 1.png";
 import SoloLogo from "../Assets/Group 22.png";
 const Landing = () => {
+  const [signUp, setSignUp] = useState(false);
+  console.log(signUp);
   return (
-    <div className='containerParent'>
-        <div className='leftPart'>
-            <div className='logo'><img src={Logo}/></div>
-            <div className='textLeftFirst'>
-              <p>Your podcast
-                will no longer
-                be just a hobby.</p>
-            </div>
-            <div className='textLeftSecond'>
-              <p>Supercharge Your Distribution
-                 using our AI assistant!</p>
-            </div>
+    <div className="containerParent">
+      <div className="leftPart">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
         </div>
-        <div className='rightPart'>
-            <div><img src={SoloLogo}/></div>
-            <div className='logoTxt'>
-              <p>Welcome to
-                <br/> <strong>Ques.AI</strong></p>
+        <div className="textLeftFirst">
+          <p>Your podcast will no longer be just a hobby.</p>
+        </div>
+        <div className="textLeftSecond">
+          <p>Supercharge Your Distribution using our AI assistant!</p>
+        </div>
+      </div>
+      <div className="rightPart">
+        <div>
+          <img src={SoloLogo} alt="only logo" />
+        </div>
+        <div className="logoTxt">
+          <p>
+            Welcome to
+            <br /> <strong>Ques.AI</strong>
+          </p>
+        </div>
+
+        {signUp ? (
+          <>
+            <form className="form">
+              <input type="email" placeholder="Email Address" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Confirm Password" />
+            </form>
+            <button type="submit">Sign Up</button>
+
+            <div className="create-account">
+              <span>Already have an account? </span>
+              <span onClick={() => setSignUp(false)} className="create-link">
+                Login
+              </span>
             </div>
-          
-            <form className='form'>
-              <input type='email' placeholder='Email Address'/>
-              <input type='password' placeholder='Password'/>
+          </>
+        ) : (
+          <>
+            <form className="form">
+              <input type="email" placeholder="Email Address" />
+              <input type="password" placeholder="Password" />
               <div>
-                <div className='checkBox'><input type='checkbox'/>Remember me</div>
-              <p>Forgot password?</p>
+                <div className="checkBox">
+                  <input type="checkbox" />
+                  Remember me
+                </div>
+                <p>Forgot password?</p>
               </div>
             </form>
-            <button type='submit'>Login</button>
-        </div>
+            <button type="submit">Login</button>
+
+            <div className="separator">
+              <div className="line"></div>
+              <span className="text">or</span>
+              <div className="line"></div>
+            </div>
+
+            <div className="create-account">
+              <span>Don't have an account? </span>
+              <span onClick={() => setSignUp(true)} className="create-link">
+                Create Account
+              </span>
+            </div>
+          </>
+        )}
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
-
+export default Landing;
